b4a134c42cb70673e45e74ea1378104e276fbd52
Hirosuke Miyajima (宮島大介) <hirosuke.miyajima@gmail.com>
2026-03-22 08:38:28 +0900

    similar fix for top

--- bot.py
+++ bot.py
@@ -63,50 +63,89 @@ finally:
 subprocess.run(["wkhtmltoimage", "--width", "800", "--crop-h", "800", f_url, "bot_temp.png"])
 subprocess.run(["right/getfed.sh", f_url, f_title])
 
-### NOTE: fka. top-news.py 
-dt = datetime.datetime.now()
-if dt.hour > 1 and dt.hour < 15: 
-    # Annyeong! 
-    lang="ko"
-    outlets = [
-        {"nm": "NEWSIS", "url": "https://www.newsis.com/RSS/sokbo.xml"},
-        {"nm": "CHOSUN1","url": "https://www.chosun.com/arc/outboundfeeds/rss/category/international/?outputType=xml"},
-        {"nm": "CHOSUN2","url": "https://www.chosun.com/arc/outboundfeeds/rss/category/culture-life/?outputType=xml"},
-        {"nm": "MK",     "url": "https://www.mk.co.kr/rss/40300001/"},
-        {"nm": "HK",     "url": "https://www.hankyung.com/feed/all-news"},
-        {"nm": "HBIZ",   "url": "https://biz.heraldcorp.com/rss/google/newsAll"},
-        {"nm": "KPOP",   "url": "https://www.heraldmuse.com/rss/google/hm_kpop"}
-    ]
-else: 
-    # Good morning, America. 
-    lang="en"
-    outlets = [
-        {"nm": "PBS",    "url": "https://www.pbs.org/newshour/feeds/rss/headlines"},
-        {"nm": "SDOT",   "url": "https://rss.slashdot.org/Slashdot/slashdotMain"},
-        {"nm": "NYT",    "url": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"},
-        {"nm": "GDN1",   "url": "https://www.theguardian.com/us-news/us-politics/rss"},
-        {"nm": "GDN2",   "url": "https://www.theguardian.com/world/middleeast/rss"},
-        {"nm": "GDN3",   "url": "https://www.theguardian.com/world/rss"},
-        {"nm": "GDNC",   "url": "https://www.theguardian.com/uk/commentisfree/rss"},
-        {"nm": "HILL",   "url": "https://thehill.com/feed/"},
-        {"nm": "MSNBC",  "url": "https://www.ms.now/rss"}
-    ]
+# top  
+outlets = [
+    {"lng": "ko", "nm": "NEWSIS", "url": "https://www.newsis.com/RSS/sokbo.xml"},
+    {"lng": "ko", "nm": "CHOSUN1","url": "https://www.chosun.com/arc/outboundfeeds/rss/category/international/?outputType=xml"},
+    {"lng": "ko", "nm": "CHOSUN2","url": "https://www.chosun.com/arc/outboundfeeds/rss/category/culture-life/?outputType=xml"},
+    {"lng": "ko", "nm": "MK",     "url": "https://www.mk.co.kr/rss/40300001/"},
+    {"lng": "ko", "nm": "HK",     "url": "https://www.hankyung.com/feed/all-news"},
+    {"lng": "ko", "nm": "HBIZ",   "url": "https://biz.heraldcorp.com/rss/google/newsAll"},
+    {"lng": "ko", "nm": "KPOP",   "url": "https://www.heraldmuse.com/rss/google/hm_kpop"},
+    {"lng": "en", "nm": "PBS",    "url": "https://www.pbs.org/newshour/feeds/rss/headlines"},
+    {"lng": "en", "nm": "SDOT",   "url": "https://rss.slashdot.org/Slashdot/slashdotMain"},
+    {"lng": "en", "nm": "NYT",    "url": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"},
+    {"lng": "en", "nm": "GDN1",   "url": "https://www.theguardian.com/us-news/us-politics/rss"},
+    {"lng": "en", "nm": "GDN2",   "url": "https://www.theguardian.com/world/middleeast/rss"},
+    {"lng": "en", "nm": "GDN3",   "url": "https://www.theguardian.com/world/rss"},
+    {"lng": "en", "nm": "GDNC",   "url": "https://www.theguardian.com/uk/commentisfree/rss"},
+    {"lng": "en", "nm": "HILL",   "url": "https://thehill.com/feed/"},
+    {"lng": "en", "nm": "MSNBC",  "url": "https://www.ms.now/rss"}
+]
 
 try: 
+    cn = sqlite3.connect("turk.db")
+    cs = cn.cursor()
+
+    cs.execute('''
+        create table if not exists mixed (
+            url text primary key, 
+            title text not null, 
+            outlet text not null, 
+            lang text not null, 
+            date text not null, 
+            used integer
+        )
+    ''')
+
     i = random.randint(0, len(outlets)-1)
     outlet_url = outlets[i]["url"]
     outlet_name = outlets[i]["nm"]
-    feed = feedparser.parse(outlet_url)
-    j = random.randint(0, 3)
-    if j > len(feed.entries)-1: 
-        j = 0
-    # we'll use this one. 
-    article = feed.entries[j]
+    outlet_lang = outlets[i]["lng"]
+    f = feedparser.parse(outlet_url)
+    now = datetime.datetime.now()
+    # this is NOT their published date. 
+    date = now.strftime("%Y-%m-%d-%H%M")
+    for entry in f.entries[:3]: 
+        cs.execute('''
+            insert or ignore into mixed 
+            (url, title, outlet, lang, date, used) 
+            values (?, ?, ?, ?, ?, ?)
+        ''', 
+        (entry.link, entry.title, outlet_name, outlet_lang, date, 0))
 
-    subprocess.run(["top/getfed.sh", outlet_name, article.link, article.title, lang])
+    cs.execute('''
+        select * from mixed 
+        where used = 0 
+        order by date DESC
+    ''')
+    feeds = cs.fetchall()
+    if (len(feeds) == 0): 
+        feed = f.entries[0]
+        f_url = feed.link
+        f_title = feed.title
+        f_outlet = outlet_name
+        f_lang = outlet_lang
+    else: 
+        feed = feeds[0]
+        f_url = feed[0]
+        f_title = feed[1]
+        f_outlet = feed[2]
+        f_lang = feed[3]
+        cs.execute('''
+            update mixed set used = 1 
+            where url = ? 
+        ''', 
+        (f_url,))
 
+    subprocess.run(["top/getfed.sh", f_outlet, f_url, f_title, f_lang])
+
+    cn.commit();
 except Exception as e: 
     print(e)
+    cn.rollback();
+finally: 
+    cn.close();
 
 try: 
     url_am = "https://mycatiskorean.blogspot.com//feeds/posts/default"
