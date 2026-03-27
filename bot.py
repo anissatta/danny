3439a24151e7c2c46a977209b40b574a28aad7ce
Hirosuke Miyajima (宮島大介) <hirosuke.miyajima@gmail.com>
2026-03-28 07:24:39 +0900

    this is what is actually running

--- bot.py
+++ bot.py
@@ -6,10 +6,27 @@ import datetime
 import random
 import sqlite3
 from kiwipiepy import Kiwi
+from PIL import Image
+import imagehash
+
+has_trump = False
 
 try: 
     cn = sqlite3.connect("turk.db")
     cs = cn.cursor()
+
+    # 26. 3. 25 
+    cs.execute('''
+        create table if not exists demo1 (
+            id integer primary key, 
+            url text not null, 
+            title text not null, 
+            lang text not null
+        )
+    ''')
+    cs.execute('''
+        delete from demo1 
+    ''')
 
     cs.execute('''
         create table if not exists feeds (
@@ -32,31 +49,83 @@ try:
             values (?, ?, ?, ?, ?)
         ''', 
         (entry.link, entry.title, 'ko', date, 0))
-
+    ###################################### 
+    #   SECRET  CODE S 2026. 3. 25 
+    ###################################### 
+    cs.execute('''
+        select * from feeds 
+        where url = "LAST" 
+    ''')
+    lasts = cs.fetchall()
+    if (len(lasts) != 0): 
+        lll = lasts[0]
+        last = lll[1]
+    else: 
+        last = ""
+    ###################################### 
+    #   SECRET  CODE E 2026. 3. 25 
+    ###################################### 
     cs.execute('''
         select * from feeds 
         where used = 0 
         order by date DESC
     ''')
     feeds = cs.fetchall()
-    if (len(feeds) == 0): 
-        # if yonhap news is unavailable as our "news" outlet, then use others for now. 
-        cs.execute('''
-            select url, title, lang from mixed 
-            where outlet = 'HBIZ' or outlet = 'KPOP' 
-            order by date DESC
-        ''')
-        feeds = cs.fetchall()
-        if (len(feeds) == 0): 
+    cs.execute('''
+        select url, title, lang from mixed 
+        where used = 0 and outlet in ('HBIZ', 'KPOP', 'PBS', 'SDOT', 'MSNBC', 'FOX') 
+        order by substr(date, 1, 13)+random() DESC
+        limit 1
+    ''')
+    feeds_alt = cs.fetchall()
+    if (len(feeds_alt) != 0 and random.randint(0, 333) == 0): 
+        prefer_alt = True
+    else: 
+        prefer_alt = False
+    ###################################### 
+    #   SECRET  CODE S 2026. 3. 25 
+    ######################################
+    if (len(feeds) != 0): 
+        fff = feeds[0]
+        print(fff[1])
+        print(last)
+        if (fff[1] == last):  
+            prefer_alt = True
+    ###################################### 
+    #   SECRET  CODE E 2026. 3. 25 
+    ######################################
+    ###################################### 
+    #   SECRET  CODE S 2026. 3. 25 
+    ###################################### 
+    #evil = now + datetime.timedelta(minutes=-20)
+    #if (len(feeds) == 0): 
+    #    cs.execute('''
+    #        delete from feeds 
+    #        where date < ? 
+    #   ''', (evil.strftime("%Y-%m-%d-%H%M"),))
+    ###################################### 
+    #   SECRET  CODE E 2026. 3. 25 
+    ######################################
+    if (len(feeds) == 0 or prefer_alt): 
+        # if yonhap news is unavailable for us now or, 
+        # we feel like using some other news outlets, 
+        # then try using one of them. 
+        if (len(feeds_alt) == 0): 
             feed = f.entries[0]
             f_url = feed.link
             f_title = feed.title
             f_lang = "ko"
         else: 
-            feed = feeds[0]
+            feed = feeds_alt[0]
             f_url = feed[0]
-            f_title = '이걸 대신 사용: ' + feed[1]
-            f_lang = feed[2]
+            #f_title = '이걸 대신 사용: ' + feed[1]
+            f_title = feed[1]
+            f_lang = feed[2]        
+            cs.execute('''
+                update mixed set used = 1 
+                where url = ? 
+            ''', 
+            (f_url,))
     else: 
         feed = feeds[0]
         f_url = feed[0]
@@ -67,49 +136,98 @@ try:
             where url = ? 
         ''', 
         (f_url,))
+        ###################################### 
+        #   SECRET  CODE S 2026. 3. 25 
+        ###################################### 
+        cs.execute('''
+            insert or ignore into feeds 
+            (url, title, lang, date, used) 
+            values (?, ?, ?, ?, ?)
+        ''', 
+        ("LAST", entry.title, 'ko', "9999-99-99-9999", 1))
+        ###################################### 
+        #   SECRET  CODE E 2026. 3. 25 
+        ###################################### 
 
     # word (noun only) count using Kiwipiepy. 
+    if f_lang == "ko": 
+        cs.execute('''
+            create table if not exists stat1 (
+                word text primary key, 
+                count integer
+            )
+        ''')
+        kiwi = Kiwi()
+        tks = kiwi.tokenize(f_title)
+        for tk in tks: 
+            if tk.tag == "NNG" or \
+               tk.tag == "NNP" or \
+               tk.tag == "NNB": 
+                w = tk.form
+                cs.execute('''
+                    insert or ignore into stat1 
+                    (word, count) 
+                    values (?, ?)
+                ''', 
+                (w, 0))
+                cs.execute('''
+                    update stat1 
+                    set count = count + 1 
+                    where word = ? 
+                ''', 
+                (w,))
+                # Trump 2.0 
+                if (w == '트럼프'): 
+                    has_trump = True
+    else: 
+        if "Trump" in f_title: 
+            has_trump = True
+
+    # 26. 3. 25 
     cs.execute('''
-        create table if not exists stat1 (
-            word text primary key, 
-            count integer
+        insert or ignore into demo1 
+        (id, url, title, lang) 
+        values (?, ?, ?, ?)
+    ''', 
+    (1, f_url, f_title, f_lang))
+
+    subprocess.run(["wkhtmltoimage", "--width", "800", "--crop-y", "160", "--crop-h", "480", f_url, "bot_temp.png"])
+    subprocess.run(["right/getfed.sh", f_url, f_title, f_lang])
+    # 26. 3. 27 
+    ihash = imagehash.phash(Image.open("bot_temp.png"))
+    # 26. 3. 27 
+    cs.execute('''
+        create table if not exists ihash (
+            hash text primary key, 
+            url text not null, 
+            date text not null
         )
     ''')
-    kiwi = Kiwi()
-    tks = kiwi.tokenize(f_title)
-    for tk in tks: 
-        if tk.tag == "NNG" or \
-           tk.tag == "NNP" or \
-           tk.tag == "NNB": 
-            w = tk.form
-            cs.execute('''
-                insert or ignore into stat1 
-                (word, count) 
-                values (?, ?)
-            ''', 
-            (w, 0))
-            cs.execute('''
-                update stat1 
-                set count = count + 1 
-                where word = ? 
-            ''', 
-            (w,))
+    cs.execute('''
+        insert or ignore into ihash 
+        (hash, url, date) 
+        values (?, ?, ?)
+    ''', 
+    (str(ihash), f_url, now.strftime("%Y-%m-%d-%H%M")))
 
     cn.commit();
+    print("**************************************** STAGE1: OK")
 except Exception as e: 
     print(e)
     cn.rollback();
 finally: 
     cn.close();
 
-subprocess.run(["wkhtmltoimage", "--width", "800", "--crop-y", "320", "--crop-h", "480", f_url, "bot_temp.png"])
-subprocess.run(["right/getfed.sh", f_url, f_title, "ko"])
+if (has_trump): 
+    subprocess.run(["composite", "-gravity", "SouthEast", "-geometry", "+0+0", "cf/trump.png", "bot_temp.png", "bot_temp.png"])
 
 # top  
 outlets = [
     {"lng": "ko", "nm": "NEWSIS", "url": "https://www.newsis.com/RSS/sokbo.xml"},
+    {"lng": "ko", "nm": "NEWSIS", "url": "https://www.newsis.com/RSS/sokbo.xml"},
     {"lng": "ko", "nm": "CHOSUN1","url": "https://www.chosun.com/arc/outboundfeeds/rss/category/international/?outputType=xml"},
     {"lng": "ko", "nm": "CHOSUN2","url": "https://www.chosun.com/arc/outboundfeeds/rss/category/culture-life/?outputType=xml"},
+    {"lng": "ko", "nm": "MK",     "url": "https://www.mk.co.kr/rss/40300001/"},
     {"lng": "ko", "nm": "MK",     "url": "https://www.mk.co.kr/rss/40300001/"},
     {"lng": "ko", "nm": "HK",     "url": "https://www.hankyung.com/feed/all-news"},
     {"lng": "ko", "nm": "HBIZ",   "url": "https://biz.heraldcorp.com/rss/google/newsAll"},
@@ -117,18 +235,24 @@ outlets = [
     {"lng": "en", "nm": "PBS",    "url": "https://www.pbs.org/newshour/feeds/rss/headlines"},
     {"lng": "en", "nm": "SDOT",   "url": "https://rss.slashdot.org/Slashdot/slashdotMain"},
     {"lng": "en", "nm": "NYT",    "url": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"},
+    {"lng": "en", "nm": "NYT",    "url": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"},
     {"lng": "en", "nm": "GDN1",   "url": "https://www.theguardian.com/us-news/us-politics/rss"},
     {"lng": "en", "nm": "GDN2",   "url": "https://www.theguardian.com/world/middleeast/rss"},
+    {"lng": "en", "nm": "GDN3",   "url": "https://www.theguardian.com/world/rss"},
     {"lng": "en", "nm": "GDN3",   "url": "https://www.theguardian.com/world/rss"},
     {"lng": "en", "nm": "GDNC",   "url": "https://www.theguardian.com/uk/commentisfree/rss"},
     {"lng": "en", "nm": "HILL",   "url": "https://thehill.com/feed/"},
     {"lng": "en", "nm": "MSNBC",  "url": "https://www.ms.now/rss"},
     {"lng": "en", "nm": "FOX",    "url": "https://moxie.foxnews.com/google-publisher/latest.xml"},
+    {"lng": "en", "nm": "FOX",    "url": "https://moxie.foxnews.com/google-publisher/latest.xml"},
     {"lng": "en", "nm": "FP",     "url": "https://foreignpolicy.com/rss"},
-    {"lng": "en", "nm": "ET",     "url": "https://www.etonline.com/news/rss"},
     {"lng": "en", "nm": "IRAN",   "url": "https://www.iranintl.com/en/feed"},
+    {"lng": "en", "nm": "IRAN",   "url": "https://www.iranintl.com/en/feed"},
+    {"lng": "en", "nm": "ET",     "url": "https://www.etonline.com/news/rss"},
     {"lng": "en", "nm": "CGTN",   "url": "https://www.cgtn.com/subscribe/rss/section/world.xml"},
     {"lng": "en", "nm": "NDTV",   "url": "https://feeds.feedburner.com/ndtvnews-latest"},
+    {"lng": "en", "nm": "NDTV",   "url": "https://feeds.feedburner.com/ndtvnews-latest"},
+    {"lng": "hi", "nm": "DB",     "url": "https://www.bhaskar.com/rss-v1--category-4587.xml"},
     {"lng": "hi", "nm": "DB",     "url": "https://www.bhaskar.com/rss-v1--category-4587.xml"},
     {"lng": "ja", "nm": "ASAHI",  "url": "https://www.asahi.com/rss/asahi/newsheadlines.rdf"},
     {"lng": "ja", "nm": "NIKKEI", "url": "https://business.nikkei.com/rss/sns/nb.rdf"}
@@ -149,21 +273,22 @@ try:
         )
     ''')
 
-    i = random.randint(0, len(outlets)-1)
-    outlet_url = outlets[i]["url"]
-    outlet_name = outlets[i]["nm"]
-    outlet_lang = outlets[i]["lng"]
-    f = feedparser.parse(outlet_url)
     now = datetime.datetime.now()
     # this is NOT their published date. 
     date = now.strftime("%Y-%m-%d-%H%M")
-    for entry in f.entries[:3]: 
-        cs.execute('''
-            insert or ignore into mixed 
-            (url, title, outlet, lang, date, used) 
-            values (?, ?, ?, ?, ?, ?)
-        ''', 
-        (entry.link, entry.title, outlet_name, outlet_lang, date, 0))
+    for j in range(4): 
+        i = random.randint(0, len(outlets)-1)
+        outlet_url = outlets[i]["url"]
+        outlet_name = outlets[i]["nm"]
+        outlet_lang = outlets[i]["lng"]
+        f = feedparser.parse(outlet_url)
+        for entry in f.entries[:3]: 
+            cs.execute('''
+                insert or ignore into mixed 
+                (url, title, outlet, lang, date, used) 
+                values (?, ?, ?, ?, ?, ?)
+            ''', 
+            (entry.link, entry.title, outlet_name, outlet_lang, date, 0))
 
     cs.execute('''
         select * from mixed 
@@ -189,7 +314,40 @@ try:
         ''', 
         (f_url,))
 
+    # 26. 3. 25 
+    cs.execute('''
+        insert or ignore into demo1 
+        (id, url, title, lang) 
+        values (?, ?, ?, ?)
+    ''', 
+    (2, f_url, f_title, f_lang))
+    # 26. 3. 27 
+    # dirty code... 
+    cs.execute('''
+        insert or ignore into demo1 
+        (id, url, title, lang) 
+        values (?, ?, ?, ?)
+    ''', 
+    (3, f_outlet, "", f_lang))
+
     subprocess.run(["top/getfed.sh", f_outlet, f_url, f_title, f_lang])
+
+    ###################################### 
+    #   SECRET  CODE S 2026. 3. 25 
+    ###################################### 
+    hago = now + datetime.timedelta(minutes=-44)
+    cs.execute('''
+        delete from feeds 
+        where date < ? 
+   ''', (hago.strftime("%Y-%m-%d-%H%M"),))
+    tdago = now + datetime.timedelta(days=-3)
+    cs.execute('''
+        delete from mixed 
+        where date < ? 
+   ''', (tdago.strftime("%Y-%m-%d-%H%M"),))
+    ###################################### 
+    #   SECRET  CODE E 2026. 3. 25 
+    ###################################### 
 
     cn.commit();
 except Exception as e: 
@@ -198,13 +356,13 @@ except Exception as e:
 finally: 
     cn.close();
 
-try: 
-    url_am = "https://mycatiskorean.blogspot.com//feeds/posts/default"
-    am = feedparser.parse(url_am)
-    url_art = am.entries[0].link
-    subprocess.run(["wkhtmltoimage", "--width", "320", "--crop-y", "321", "--crop-h", "480", url_art, "fright.png"])
-except Exception as e: 
-    print(e)
+#try: 
+#    url_am = "https://mycatiskorean.blogspot.com//feeds/posts/default"
+#    am = feedparser.parse(url_am)
+#    url_art = am.entries[0].link
+#    subprocess.run(["wkhtmltoimage", "--width", "320", "--crop-y", "321", "--crop-h", "480", url_art, "fright.png"])
+#except Exception as e: 
+#    print(e)
 
 # 26. 3. 22 
 try: 
@@ -213,7 +371,7 @@ try:
 
     cs.execute('''
         select * from mixed
-        where lang = 'en' and used = 1 
+        where lang = 'en' 
         order by random() 
         limit 10
     ''')
